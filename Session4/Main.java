328d8a7499889b237890dacba684889241c1a09f
YanethM <yaneth.mejiar@autonoma.edu.co>
2024-03-08 10:47:06 -0500

    Resolve the first point

---
+++ Session4/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Exercise1 instance_exercise1 = new Exercise1();
+        System.out.println(instance_exercise1.sumaArrayNumeros());
+    }
+}
